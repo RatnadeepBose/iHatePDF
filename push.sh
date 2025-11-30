bfb1f169631a8216163b1a82dc1f1000a5e76b99
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2025-12-01 00:39:38 +0530

    added some important links

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-cd "C:\Users\boser\Downloads\i hate pdf" && git add . && git commit -m "adjustments for both mobile and desktop" && git push origin main
+cd "C:\Users\boser\Downloads\i hate pdf" && git add . && git commit -m "added some important links" && git push origin main
